e33f4494840d7e3478b95f33ef3f64c510783db6
habbaza <habbazahubbal@gmail.com>
2025-01-20 14:16:48 +0700

    add projectUrl

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,49 +5,49 @@ const projects = [
         imgSrc: '/projects/project-1.png',
         title: 'Sampahin : Waste Classification Web App',
         tags: ['TensorFlowJS', 'ExpressJS', 'ReactJS', 'PostgreSQL', 'Google Cloud Platform'],
-        projectLink: ''
+        projectLink: 'https://sampahin.netlify.app/'
     },
     {
         imgSrc: '/projects/project-2.png',
         title: 'SkinSense API : Early Detection of Melanoma Cancer API',
         tags: ['TensorFlowJS', 'ExpressJS', 'PostgreSQL', 'Google Cloud Platform'],
-        projectLink: ''
+        projectLink: 'https://github.com/habbazettt/SkinSense-App'
     },
     {
         imgSrc: '/projects/project-3.png',
         title: 'DoIt : Cloud Based Task Scheduler',
         tags: ['TypeScript', 'NextJS', 'ExpressJS', 'MongoDB', 'Google Cloud Platform'],
-        projectLink: ''
+        projectLink: 'https://doitbyhabbaza.netlify.app/'
     },
     {
         imgSrc: '/projects/project-4.png',
         title: 'KKN 410 Cibitung UIN Sunan Gunung Djati Bandung Web Profile',
         tags: ['ReactJS', 'TailwindCSS'],
-        projectLink: ''
+        projectLink: 'https://github.com/habbazettt/kkn-410-app'
     },
     {
         imgSrc: '/projects/project-5.png',
         title: 'Mahad Tahfidz UIN Sunan Gunung Djati Bandung Web Profile',
         tags: ['ReactJS', 'TailwindCSS'],
-        projectLink: ''
+        projectLink: 'https://mahadtahfidz.netlify.app/'
     },
     {
         imgSrc: '/projects/project-6.png',
-        title: 'PT Gemilang Radim Utama Web Profile',
+        title: 'PT Gemilang Radim Utama Company Profile',
         tags: ['ReactJS', 'TailwindCSS'],
-        projectLink: ''
+        projectLink: 'https://radimgroup.netlify.app/'
     },
     {
         imgSrc: '/projects/project-7.png',
         title: 'Image and Audio Processing Web App',
         tags: ['Flask', 'TailwindCSS'],
-        projectLink: ''
+        projectLink: 'https://image-audio-processing-habbaza.netlify.app/'
     },
     {
         imgSrc: '/projects/project-8.png',
         title: 'Pokedex Web App',
         tags: ['NextJS'],
-        projectLink: ''
+        projectLink: 'https://github.com/habbazettt/pokedex-app'
     }
 ];
 
